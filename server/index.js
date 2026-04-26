00291640e028c7f759497d1eeaa88c0f52071e25
siddhant266 <sid.wskr.03@gmail.com>
2026-04-27 01:12:29 +0530

    Handle OPTIONS preflight for CORS

--- server/index.js
+++ server/index.js
@@ -13,8 +13,11 @@ const PORT = process.env.PORT || 5000;
 // Explicit CORS headers for Render deployment
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 app.use(express.json());
