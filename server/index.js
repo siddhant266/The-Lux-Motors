253df2ab4d473fa0588b1c4cd49095fb3820eb13
siddhant266 <sid.wskr.03@gmail.com>
2026-04-27 00:59:25 +0530

    Fix CORS policy to allow Vercel frontend

--- server/index.js
+++ server/index.js
@@ -11,11 +11,7 @@ const app  = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:3000'];
-if (process.env.FRONTEND_URL) {
-    allowedOrigins.push(process.env.FRONTEND_URL);
-}
-app.use(cors({ origin: allowedOrigins }));
+app.use(cors());
 app.use(express.json());
 
 // Routes
